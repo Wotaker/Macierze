e580fea8d90167044fbd7cd0d9d7e8c7cd9e686f
gregori0o <glegeza@wp.pl>
2023-03-11 22:02:02 +0100

    Implement experiments, first version of report

--- prog01/classic_strasen.py
+++ prog01/classic_strasen.py
@@ -61,3 +61,47 @@ def multiply_strassen(A: Matrix, B: Matrix):
         return C
 
     return strassen(A, B, A.shape[0])
+
+
+def multiply_strassen_with_classic(A: Matrix, B: Matrix, size_classic: int = 1):
+    """
+    Multiplies `A` times `B` with a Strassen algorithm, where
+    both `A` and `B` are square `n x n` matrices.
+    If submatrix size is less or equal than `size_classic` uses `multiply_classic`.
+    """
+
+    def strassen(A: Matrix, B: Matrix, n: int):
+
+        if n == 1:
+            return A * B
+        elif n <= size_classic:
+            return multiply_classic(A, B)
+        
+        m = n // 2
+
+        A11 = A[:m, :m]
+        A12 = A[:m, m:]
+        A21 = A[m:, :m]
+        A22 = A[m:, m:]
+
+        B11 = B[:m, :m]
+        B12 = B[:m, m:]
+        B21 = B[m:, :m]
+        B22 = B[m:, m:]
+
+        P1 = strassen(A11 + A22, B11 + B22, m)
+        P2 = strassen(A21 + A22, B11, m)
+        P3 = strassen(A11, B12 - B22, m)
+        P4 = strassen(A22, B21 - B11, m)
+        P5 = strassen(A11 + A12, B22, m)
+        P6 = strassen(A21 - A11, B11 + B12, m)
+        P7 = strassen(A12 - A22, B21 + B22, m)
+
+        C = np.concatenate((
+            np.concatenate((P1 + P4 - P5 + P7, P3 + P5), axis=1),
+            np.concatenate((P2 + P4, P1 - P2 + P3 + P6), axis=1)
+        ), axis=0)
+
+        return C
+
+    return strassen(A, B, A.shape[0])
